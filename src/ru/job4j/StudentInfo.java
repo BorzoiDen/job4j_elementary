d6be5c9bda9ebdd41fe36e19a99e1e722bba32c8
Denis <89137247888@mail.ru>
2020-06-06 15:22:41 +0700

    3.3. Разбор первой программы. [#298403]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Borozenets Denis");
+        System.out.println("06.06.20");
+    }
+}
